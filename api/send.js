03d9faa344cb7ed7a800394d1fa924b70500b0b7
constructioncert59-a11y <constructioncert59@gmail.com>
2026-04-24 12:11:05 +0530

    Update send.js

--- api/send.js
+++ api/send.js
@@ -1,6 +1,6 @@
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.re_gxfHZBX4_9NaiH6l1bYynd4gWXjhRpYg);
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
