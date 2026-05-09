20461fd859f36eeca6a3ca2217ce5dbe1c0ff6eb
constructioncert59-a11y <constructioncert59@gmail.com>
2026-05-09 13:41:36 +0530

    Update send.js

--- api/send.js
+++ api/send.js
@@ -21,8 +21,7 @@ export default async function handler(req, res) {
     // ADMIN EMAIL
     await resend.emails.send({
       from: "onboarding@resend.dev",
-      to: "applyecs4@gmail.com
-",
+      to: "applyecs4@gmail.com",
       subject: "🔥 New ECS Booking",
       html: `<h2>New Booking</h2>${allDataHtml}`
     });
